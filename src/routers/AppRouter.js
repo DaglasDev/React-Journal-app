e4b63057a92368e589802a2e656de08df421094f
Daglas Ruiz <daglasruizdev@gmail.com>
2021-04-16 12:47:51 -0400

    feat: replace BrowserRouter to HashRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Redirect, Switch } from "react-router-dom";
+import { HashRouter as Router, Redirect, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 import { firebase } from "../firebase/firebaseConfig.js";
